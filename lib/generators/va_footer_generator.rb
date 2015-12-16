f079ba60c24137a8e94fec58ebdfe016cd3c82e6
Emily Wright-Moore <emily@codeforamerica.org>
2015-12-16 13:03:07 -0800

    update header and footer and add legal section, move header/footer to vendor directory

--- lib/generators/va_footer_generator.rb
+++ lib/generators/va_footer_generator.rb
@@ -2,6 +2,6 @@ class VaFooterGenerator < Rails::Generators::Base
   source_root File.expand_path("../files", __FILE__)
 
   def create_footer_file
-    copy_file "_footer.html.erb", "app/views/common/_footer.html.erb"
+    copy_file "_footer.html.erb", "app/vendor/va_common/_footer.html.erb"
   end
 end
--- lib/generators/va_header_generator.rb
+++ lib/generators/va_header_generator.rb
@@ -2,6 +2,6 @@ class VaHeaderGenerator < Rails::Generators::Base
   source_root File.expand_path("../files", __FILE__)
 
   def create_header_file
-    copy_file "_header.html.erb", "app/views/common/_header.html.erb"
+    copy_file "_header.html.erb", "app/vendor/va_common/_header.html.erb"
   end
 end
